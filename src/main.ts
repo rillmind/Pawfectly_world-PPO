b03ab0d6e71b97a93bf0fd198696e4da675067b2
Raul Holanda Lopes <raulzc00@gmail.com>
2023-11-03 15:45:55 -0300

    feat: testando cors 9

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "*",
+    origin: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     credentials: true,
   });
