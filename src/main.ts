0e63620b5454183505be44398df5643b2569d002
Raul Holanda Lopes <raulzc00@gmail.com>
2023-11-03 14:21:56 -0300

    feat: testando cors 7

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,18 @@ async function bootstrap() {
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     credentials: true,
   });
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Ou '*' para permitir qualquer origem
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, DELETE, OPTIONS"
+    );
+    next();
+  });
   await app.listen(2306);
 }
 bootstrap();
