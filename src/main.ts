d2fb4993544ca0492b28929b8a7dd7ff206bb7da
Raul Holanda Lopes <raulzc00@gmail.com>
2023-11-03 15:43:08 -0300

    feat: testando cors 8

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,9 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     credentials: true,
-  });
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Ou '*' para permitir qualquer origem
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, DELETE, OPTIONS"
-    );
-    next();
   });
   await app.listen(2306);
 }
