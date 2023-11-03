b7f6a7c50b263e5d7c9d296fe70b58f40d5cbc3e
Raul Holanda Lopes <raulzc00@gmail.com>
2023-11-03 12:40:05 -0300

    feat: testando cors 2

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create(AppModule);
+  app.use(cors({
     cors: {
-      origin: true,
+      origin: 'http://localhost:3000',
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
       credentials: true,
     },
-  });
+  }))
   await app.listen(2306);
 }
 bootstrap();
