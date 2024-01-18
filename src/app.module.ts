cc297da02ba2620c58a03ad7a7f8c2e47b69d832
Raul Holanda Lopes <raulzc00@gmail.com>
2024-01-18 15:59:21 -0300

    fix: ajustando app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,22 +4,20 @@ import { AuthModule } from "./auth/auth.module";
 import { ConfigModule } from "@nestjs/config";
 import { AnimalModule } from "./animal/animal.module";
 import { MongooseModule } from "@nestjs/mongoose";
-import { PostModule } from './post/post.module';
-import { AdoptionModule } from './adoption/adoption.module';
-import { VaccinationModule } from './vaccination/vaccination.module';
-import { UploadModule } from './upload/upload.module';
+import { PostModule } from "./post/post.module";
+import { AdoptionModule } from "./adoption/adoption.module";
+import { VaccinationModule } from "./vaccination/vaccination.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: ".env", isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot(process.env.ATLAS_URI),
     AnimalModule,
     UserModule,
     AuthModule,
     PostModule,
     AdoptionModule,
     VaccinationModule,
-    UploadModule,
   ],
 })
 export class AppModule {}
